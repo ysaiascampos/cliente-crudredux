43d6a8234477bb2b9a62d39f3692d7778bdcbaa0
ysaiascampos <ysaiascampos@gmail.com>
2020-10-15 21:52:59 -0300

    configurando redux

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import EditarProducto from './components/EditarProducto';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Productos} />
-          <Route exact path="/productos/nuevo" component={NuevoProducto} />
-          <Route exact path="/productos/editar/:id" component={EditarProducto} />
-        </Switch>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route exact path="/productos/editar/:id" component={EditarProducto} />
+          </Switch>
 
-      </div>
+        </div>
+      </Provider>
     </Router>
   );
 }
