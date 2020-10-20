8182379fe5b6b2458c0b7b4da3c0566600510613
ysaiascampos <ysaiascampos@gmail.com>
2020-10-19 21:30:54 -0300

    redux

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { crearNuevoProductoAction } from '../actions/productoActions'
+
 const NuevoProducto = () => {
+
+    //state del componente
+    const [nombre, setNombre] = useState('');
+    const [precio, setPrecio] = useState(0)
+
+
+    // utilizar el use dispatch y te crea una función
+    const dispatch = useDispatch();
+
+    //llamar el action de productoAction
+    const agregarProducto = producto => dispatch(crearNuevoProductoAction(producto));
+
+    //cuando el usuario haga submit
+    const submitNuevoProducto = e => {
+        e.preventDefault();
+
+        // validar formulario
+        if(nombre.trim() === '' || precio <= 0){
+            return;
+        }
+
+        // si no hay errores
+
+        // crear el nuevo producto
+        agregarProducto({
+            nombre,
+            precio
+        });
+    }
+
+
     return ( 
         <div className="row justify-content-center">
             <div className="col-md-8">
@@ -8,7 +42,9 @@ const NuevoProducto = () => {
                         <h2 className="text-center mb-4 font-weight-bold">
                             Agregar Nuevo Producto
                         </h2>
-                        <form>
+                        <form
+                            onSubmit={submitNuevoProducto}
+                        >
                             <div className="form-group">
                                 <label>Nombre Producto</label>
                                 <input 
@@ -16,6 +52,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Nombre Producto"
                                     name="nombre"
+                                    value={nombre}
+                                    onChange={e => setNombre(e.target.value)}
                                 />
                             </div>
                             <div className="form-group">
@@ -25,6 +63,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Precio Producto"
                                     name="precio"
+                                    value={precio}
+                                    onChange={e => setPrecio(Number(e.target.value))}
                                 />
                             </div>
                             <button
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,3 +1,10 @@
+
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+
 // cada reducer tiene su propio state
 const initialState = {
     productos: [],
